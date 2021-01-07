4bc219463affb330b4741a6b9d0436d73d1f719a
Kazuma7 <deetbeet1234@yahoo.co.jp>
2021-01-08 01:09:04 +0900

    多点に対する光強度の均一性を追加

--- cgh-gs.py
+++ cgh-gs.py
@@ -1,16 +1,49 @@
 # -*- coding: utf-8 -*-
-
 import cv2
 import numpy as np
 
-hr = 150                                                                       
-wr = 150     
 
-def root_mean_squared_error(pred, true):                                       #RMSE評価関数
+#RMSE評価関数
+def root_mean_squared_error(pred, true):
+    hr = 150
+    wr = 150                                       
     pred_area = pred[int(height/2-hr):int(height/2+hr),int(width/2-wr):int(width/2+wr)]
     true_area = true[int(height/2-hr):int(height/2+hr),int(width/2-wr):int(width/2+wr)]
     mse = np.power(true_area - pred_area, 2).mean()
     return np.sqrt(mse)
+
+#多点に対する光の強度の均一性
+def check_uniformity(u_int, target):
+    u_int = u_int / np.max(u_int)
+    maxi = np.max(u_int[target == 1])
+    mani = np.min(u_int[target == 1])
+    uniformity = 1 - (maxi - mini) / (maxi + mini)
+    return uniformity
+
+#正規化
+def normalization(origin):
+    maxi = np.max(origin)
+    mini = np.min(origin)
+    norm = ((origin - mini) / (maxi - mini))
+    return norm
+
+#ホログラム画像生成
+def hologram(phase):
+    holophase = np.where(phase < 0, phase + 2 * np.pi, phase)
+    #条件分岐 np.where(condition x y)
+    p_max = np.max(phase)
+    p_min = np.min(phase)
+    holo = ((phase - p_min) / (p_max - p_min)) * 255
+    holo = holo.astype("uint8")
+
+#再生像生成
+def reconstruct(norm_int):
+    rec = norm_int * 255
+    rec = rec.astype("uint8")
+    return rec
+
+
+
 
 target = cv2.imread("img/target1.bmp", 0)
 # cv2.imshow("target", target)
@@ -36,20 +69,14 @@ for num in range(iteration):
     u = np.fft.fft2(u)
     u = np.fft.fftshift(u)
 
-
     u_abs = np.abs(u)
     u_int = u_abs ** 2
-
-    maxi = np.max(u_int)
-    mini = np.min(u_int)
-
-    norm = ((u_int - mini) / (maxi - mini))
+    norm_int = normalization(u_int)
 
     #再生像の元
     norm_int = norm
 
-    rec = norm_int * 255
-    rec = rec.astype("uint8")
+    rec = reconstruct(norm_int)
 
     phase = np.angle(u)
 
@@ -64,12 +91,7 @@ for num in range(iteration):
     #CGHの元を生成
     phase = np.angle(u)
 
-    holophase = np.where(phase < 0, phase + 2 * np.pi, phase)
-    #条件分岐 np.where(condition x y)
-    p_max = np.max(phase)
-    p_min = np.min(phase)
-    holo = ((phase - p_min) / (p_max - p_min)) * 255
-    holo = holo.astype("uint8")
+    holo = hologram(phase)
 
     holo_name = "holo"
     rec_name = "rec"
@@ -86,7 +108,8 @@ for num in range(iteration):
     # cv2.waitKey(0)
 
     mse = root_mean_squared_error(norm_int, target)
-    print(mse)       
+    print(mse)
+    print(check_uniformity(u_int, target))
 
 
 
